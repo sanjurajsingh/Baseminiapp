6c9bc51c6aa1ab5331b7730557ebd7e50f4155c2
Sanju Singh <119477810+sanjurajsingh@users.noreply.github.com>
2026-02-01 00:10:13 +0530

    Update ContractRegistry.sol
    feat: add register function

--- contracts/contracts/contracts/ContractRegistry.sol
+++ contracts/contracts/contracts/ContractRegistry.sol
@@ -5,3 +5,6 @@ contracts ContractRegistry {}
 contract ContractRegistry {
     mapping(string => address) public registry;
 }
+function register(string memory name, address addr) public {
+    registry[name] = addr;
+}
